48314d5bf7266e10019a0e51e544f61279ae74d2
yousi <yousifwilliam0123@gmail.com>
2022-09-11 11:55:17 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const { response } = require('express');
+const e = require('express');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const matches = require("./Data/Matches.json");
+
+app.use(express.json());
+
+/* 
+    Incase you are using mongodb atlas database uncomment below line
+    and replace "mongoAtlasUri" with your mongodb atlas uri.
+*/
+// mongoose.connect( mongoAtlasUri, {useNewUrlParser: true, useUnifiedTopology: true})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.post('/matches/', (request, response) => {
+    if (request.body.Data) {
+        
+    } else {
+        response.status(400).send("Unable to retrieve Data!")
+    }
+})
+
+app.get('/players', (request, response) => {
+    response.send()
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+});
